133ee3c88f86b310719da57549b39a9dbb98eab6
Westin Smith <161383199+TuriGitGit@users.noreply.github.com>
2025-09-08 04:39:53 -0400

    Update bullshit.h
    halfway there

--- pufferlib/ocean/bullshit/bullshit.h
+++ pufferlib/ocean/bullshit/bullshit.h
@@ -1,5 +1,4 @@
-#ifndef BULLSHIT_H
-#define BULLSHIT_H
+#pragma once
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -23,16 +22,22 @@
 #define ACTIONS_SIZE (MAX_RANK * MAX_PLAY_COUNT + 1)
 #define OBS_SIZE (31 + ACTIONS_SIZE)   // 31 = 13+13+2+1+1+1
 
-#define MAX_PILE_CARDS 52
-#define MAX_HAND_CARDS 52
+#define DECK_SIZE 52
 
-#define TICK_RATE (1.0f/60.0f)
+static const int DECK[DECK_SIZE] = {
+    1,1,1,1, 2,2,2,2, 3,3,3,3,
+    4,4,4,4, 5,5,5,5, 6,6,6,6,
+    7,7,7,7, 8,8,8,8, 9,9,9,9,
+    10,10,10,10,  11,11,11,11,
+    12,12,12,12,  13,13,13,13
+};
+
 #define MAX_EPISODE_LENGTH 1000
 
-const Color BULL_RED = (Color){187, 0, 0, 255};
-const Color BULL_BLUE = (Color){0, 102, 187, 255};
-const Color BULL_WHITE = (Color){241, 241, 241, 241};
-const Color BULL_BG = (Color){12, 20, 30, 255};
+const Color PUFF_RED = (Color){187, 0, 0, 255};
+const Color PUFF_CYAN = (Color){0, 187, 187, 255};
+const Color PUFF_WHITE = (Color){241, 241, 241, 241};
+const Color PUFF_BACKGROUND = (Color){6, 24, 24, 255};
 
 typedef struct Log Log;
 struct Log {
@@ -45,29 +50,10 @@ struct Log {
 typedef struct Client Client;
 typedef struct CBullshit CBullshit;
 struct CBullshit {
-    /* RL interface pointers (these will be set by bindings, but allocated for standalone run) */
-    float* observations;      // length OBS_SIZE
-    int* actions;             // length 1 (action index)
-    float* rewards;           // length 1
-    unsigned char* terminals; // length 1
-
-    /* Game state */
-    int deck[52];
-    int deck_index;
-
-    int hands[2][MAX_RANK+1];   // counts of ranks per player (1..13), index 0 unused for convenience
-    int hand_totals[2];
-
-    int pile[MAX_PILE_CARDS];
-    int pile_size;
-
-    int last_declared_rank;
-    int last_declared_count;
-    int can_call; /* whether CALL_BS is allowed right now */
-
-    int action_masks[ACTIONS_SIZE];
-
-    /* bookkeeping & logging */
+    float* observations;
+    int* actions;
+    float* rewards;
+    unsigned char* terminals;
     Log log;
     int game_over;
     int tick;
@@ -75,37 +61,27 @@ struct CBullshit {
     float episode_return;
     float episode_length;
 
-    /* rendering / client */
+    int deck[DECK_SIZE];
+    int deck_index;
+
+    int hands[2][MAX_RANK+1];
+    int hand_totals[2];
+
+    int pile[DECK_SIZE];
+    int pile_size;
+
+    int last_declared_rank;
+    int last_declared_count;
+    int can_call;
+
+    int action_masks[ACTIONS_SIZE];
+
     int width;
     int height;
     Client* client;
 };
 
-/* ------------------------------ Prototypes -------------------------------- */
-void add_log(CBullshit* env);
-
-void init_cbullshit(CBullshit* env);
-void allocate_cbullshit(CBullshit* env);
-void c_reset(CBullshit* env);
-void compute_observations(CBullshit* env);
-void update_action_masks(CBullshit* env);
-
-void place_play(CBullshit* env, int declared_rank, int declared_count, int player_idx);
-int get_bot_play(CBullshit* env);
-int get_bot_call(CBullshit* env);
-void check_call_resolution(CBullshit* env, int caller_idx);
-
-void c_step(CBullshit* env);
-
-Client* make_client_bs(int width, int height);
-void c_render(CBullshit* env);
-
-void c_close(CBullshit* env);
-void free_allocated_cbullshit(CBullshit* env);
-
-/* --------------------------- Implementations ------------------------------ */
-
-void add_log(CBullshit* env) {
+void addLog(CBullshit* env) {
     env->log.perf += env->perf;
     env->log.episode_return += env->episode_return;
     env->log.episode_length += env->episode_length;
@@ -113,8 +89,8 @@ void add_log(CBullshit* env) {
 }
 
 /* Fisher–Yates shuffle */
-static void shuffle_deck(int *deck, int n) {
-    for (int i = n - 1; i > 0; i--) {
+static void shuffle(int *deck) {
+    for (int i = DECK_SIZE - 1; i > 0; i--) {
         int j = rand() % (i + 1);
         int tmp = deck[i];
         deck[i] = deck[j];
@@ -122,14 +98,10 @@ static void shuffle_deck(int *deck, int n) {
     }
 }
 
-void init_cbullshit(CBullshit* env) {
+void initCBullShit(CBullshit* env) {
     /* Initialize deck: 4 of each rank 1..13 */
-    int idx = 0;
-    for (int r = 1; r <= MAX_RANK; r++) {
-        for (int c = 0; c < 4; c++) {
-            env->deck[idx++] = r;
-        }
-    }
+    memcpy(env->deck, DECK, sizeof(DECK));
+
     env->deck_index = 0;
     env->pile_size = 0;
     env->last_declared_rank = 0;
@@ -151,60 +123,53 @@ void init_cbullshit(CBullshit* env) {
     for (int i = 0; i < ACTIONS_SIZE; i++) env->action_masks[i] = 0;
 }
 
-/* allocate buffers for standalone runs; binding layer will overwrite these pointers */
-void allocate_cbullshit(CBullshit* env) {
-    /* allocate observations/actions/rewards/terminals for standalone C */
-    env->observations = (float*)calloc(OBS_SIZE, sizeof(float));
+/* Stolen from TT */
+void allocateCBullShit(CBullshit* env) {
     env->actions = (int*)calloc(1, sizeof(int));
-    env->rewards = (float*)calloc(1, sizeof(float));
+    env->observations = (float*)calloc(OBS_SIZE, sizeof(float));
     env->terminals = (unsigned char*)calloc(1, sizeof(unsigned char));
-
-    init_cbullshit(env);
+    env->rewards = (float*)calloc(1, sizeof(float));
+    initCBullShit(env);
 }
 
-/* Free internal allocated buffers; binding may free the struct itself */
+/* this should be safer then TT's implementation but who knows ¯\_(ツ)_/¯ */
 void c_close(CBullshit* env) {
-    if (env->client != NULL) {
-        CloseWindow();
-        env->client = NULL;
-    }
-    /* free any arrays allocated in allocate_cbullshit */
-    if (env->observations) { free(env->observations); env->observations = NULL; }
-    if (env->actions) { free(env->actions); env->actions = NULL; }
-    if (env->rewards) { free(env->rewards); env->rewards = NULL; }
-    if (env->terminals) { free(env->terminals); env->terminals = NULL; }
+    if (env->client != NULL)closeClient(env->client); env->client = NULL;
+    free(env->observations); env->observations = NULL;
+    free(env->actions); env->actions = NULL;
+    free(env->rewards); env->rewards = NULL;
+    free(env->terminals); env->terminals = NULL;
 }
 
+/* random bullshit go! */
 void free_allocated_cbullshit(CBullshit* env) {
     c_close(env);
-    /* Note: if binding allocated the struct (VecEnv), binding is responsible for free(env) */
 }
 
-/* Reset: shuffle deck, deal evenly to two players, reset pile and flags */
 void c_reset(CBullshit* env) {
-    init_cbullshit(env);
-    shuffle_deck(env->deck, 52);
+    initCBullShit(env);
+    shuffle(env->deck);
     env->deck_index = 0;
 
-    /* deal alternately to player 0 and 1 */
-    int p = 0;
-    for (int i = 0; i < 52; i++) {
-        int r = env->deck[i];
-        env->hands[p][r] += 1;
-        env->hand_totals[p] += 1;
-        p = 1 - p;
+    for (int i = 0; i < DECK_SIZE; i+=2) {
+        int r0 = env->deck[i];
+        int r1 = env->deck[i+1];
+        env->hands[0][r0] += 1;
+        env->hand_totals[0] += 1;
+        env->hands[1][r1] += 1;
+        env->hand_totals[1] += 1;
     }
-    env->deck_index = 52; /* deck fully dealt */
+
+    env->deck_index = DECK_SIZE;
     env->pile_size = 0;
     env->last_declared_rank = 0;
     env->last_declared_count = 0;
+    computeObs(env); //  i think it goes here but my brain is dying
     env->can_call = 0;
     env->terminals[0] = 0;
     env->rewards[0] = 0.0f;
     env->episode_length = 0;
     env->episode_return = 0.0f;
-
-    compute_observations(env);
 }
 
 /* Observation layout:
@@ -217,7 +182,22 @@ void c_reset(CBullshit* env) {
    30: last_declared_count
    31.. : action masks (ACTIONS_SIZE floats)
 */
-void compute_observations(CBullshit* env) {
+
+/* Observation supposed to be layout:
+   0..12: player0 hand per-rank (1..13)
+   13..25: claims history (1..13)
+   26..38: guarnteed history (1..13) // this is what the nn placed and what has been revealed
+   39: player0 total
+   40: player1 total
+   41: player2 total
+   42: player3 total
+   43: pile size
+   44..56: last_declared_rank
+   57: last_declared_count
+   58..110 : action masks (53 floats)
+*/
+
+void computeObs(CBullshit* env) {
     int idx = 0;
     for (int r = 1; r <= MAX_RANK; r++) env->observations[idx++] = (float)env->hands[0][r];
     for (int r = 1; r <= MAX_RANK; r++) env->observations[idx++] = (float)env->hands[1][r];
@@ -230,7 +210,7 @@ void compute_observations(CBullshit* env) {
 }
 
 /* Update masks: prevent playing more cards than you have; CALL_BS only if can_call */
-void update_action_masks(CBullshit* env) {
+void updateActionMasks(CBullshit* env) {
     int my_total = env->hand_totals[0];
     for (int a = 0; a < MAX_RANK * MAX_PLAY_COUNT; a++) {
         int declared_count = (a % MAX_PLAY_COUNT) + 1;
@@ -253,13 +233,13 @@ static void add_cards_to_hand(CBullshit* env, int player_idx, int *cards, int n)
 
 /* Place a play: deterministically remove declared_count cards from player and append their real ranks to pile.
    Removal rule: take from lowest rank upward until declared_count removed. */
-void place_play(CBullshit* env, int declared_rank, int declared_count, int player_idx) {
+void placeCards(CBullshit* env, int declared_rank, int declared_count, int player_idx) {
     int removed = 0;
     for (int r = 1; r <= MAX_RANK && removed < declared_count; r++) {
         while (env->hands[player_idx][r] > 0 && removed < declared_count) {
             env->hands[player_idx][r] -= 1;
             env->hand_totals[player_idx] -= 1;
-            if (env->pile_size < MAX_PILE_CARDS) {
+            if (env->pile_size < DECK_SIZE) {
                 env->pile[env->pile_size++] = r;
             }
             removed++;
@@ -272,7 +252,7 @@ void place_play(CBullshit* env, int declared_rank, int declared_count, int playe
 }
 
 /* Simple bot heuristics */
-int get_bot_play(CBullshit* env) {
+int botAct(CBullshit* env) {
     int bot = 1;
     int ranks_with_cards[MAX_RANK];
     int n = 0;
@@ -287,7 +267,7 @@ int get_bot_play(CBullshit* env) {
     return action;
 }
 
-int get_bot_call(CBullshit* env) {
+int botCallBS(CBullshit* env) {
     float p = 0.12f;
     if (env->last_declared_count >= 3) p = 0.35f;
     float r = (float)rand() / (float)RAND_MAX;
@@ -297,7 +277,7 @@ int get_bot_call(CBullshit* env) {
 /* Caller resolves a BS call. caller_idx is the calling player (0 human, 1 bot).
    If the last declared segment is entirely equal to declared_rank -> caller was wrong -> caller picks up pile.
    Otherwise the play_player picks up the pile. */
-void check_call_resolution(CBullshit* env, int caller_idx) {
+void resolveCall(CBullshit* env, int caller_idx) {
     if (env->pile_size == 0 || env->last_declared_count == 0) {
         env->can_call = 0;
         return;
@@ -305,17 +285,18 @@ void check_call_resolution(CBullshit* env, int caller_idx) {
 
     int play_player = 1 - caller_idx;
     int declared = env->last_declared_rank;
-    int declared_count = env->last_declared_count;
-    int start_idx = env->pile_size - declared_count;
+    int start_idx = env->pile_size - env->last_declared_count;
     if (start_idx < 0) start_idx = 0;
 
     int all_match = 1;
-    for (int i = start_idx; i < env->pile_size; i++) {
-        if (env->pile[i] != declared) { all_match = 0; break; }
+    int* p = env->pile[start_idx];
+    int* e = env->pile[env->pile_size];
+    for (; p < e; p++) {
+        if (*p != declared) { all_match = 0; break; }
     }
 
     /* copy pile */
-    int temp[MAX_PILE_CARDS];
+    int temp[DECK_SIZE];
     int ntemp = env->pile_size;
     for (int i = 0; i < env->pile_size; i++) temp[i] = env->pile[i];
 
@@ -358,8 +339,10 @@ void check_call_resolution(CBullshit* env, int caller_idx) {
    - updates rewards, observations, masks
 */
 void c_step(CBullshit* env) {
+    // START copy from TT ========================================
     env->episode_length += 1;
     env->rewards[0] = 0.0f;
+    int action = env->actions[0];
 
     if (env->episode_length >= MAX_EPISODE_LENGTH) {
         env->game_over = 1;
@@ -369,19 +352,19 @@ void c_step(CBullshit* env) {
 
     if (env->game_over == 1) {
         env->perf = (env->hand_totals[0] == 0) ? 1.0f : 0.0f;
-        add_log(env);
+        addLog(env);
         c_reset(env);
         return;
     }
+    // END copy from TT ===========================================
 
-    int action = env->actions[0];
 
     /* invalid action guard */
     if (action < 0 || action >= ACTIONS_SIZE) {
         env->episode_return -= 0.1f; env->rewards[0] -= 0.1f;
     } else if (action == CALL_BS_ACTION) {
         if (env->can_call) {
-            check_call_resolution(env, 0);
+            resolveCall(env, 0);
         } else {
             env->episode_return -= 0.05f; env->rewards[0] -= 0.05f;
         }
@@ -392,19 +375,19 @@ void c_step(CBullshit* env) {
         if (declared_count > env->hand_totals[0] || env->hand_totals[0] == 0) {
             env->episode_return -= 0.1f; env->rewards[0] -= 0.1f;
         } else {
-            place_play(env, declared_rank, declared_count, 0);
+            placeCards(env, declared_rank, declared_count, 0);
 
-            /* Bot may call or play */
+            /* Bot may call or play */ // TODO: make multiple different bots
             if (env->can_call) {
-                if (get_bot_call(env)) {
-                    check_call_resolution(env, 1);
+                if (botCallBS(env)) {
+                    resolveCall(env, 1);
                 } else {
-                    int bot_action = get_bot_play(env);
+                    int bot_action = botAct(env);
                     int bot_rank = (bot_action / MAX_PLAY_COUNT) + 1;
                     int bot_count = (bot_action % MAX_PLAY_COUNT) + 1;
                     if (bot_count > env->hand_totals[1]) bot_count = env->hand_totals[1] > 0 ? 1 : 0;
                     if (bot_count > 0) {
-                        place_play(env, bot_rank, bot_count, 1);
+                        placeCards(env, bot_rank, bot_count, 1);
                         /* after bot play, human can CALL_BS on next step */
                     }
                 }
@@ -426,14 +409,14 @@ void c_step(CBullshit* env) {
         }
     }
 
-    update_action_masks(env);
-    compute_observations(env);
+    updateActionMasks(env);
+    computeObs(env);
 }
 
 /* Minimal client for rendering */
 struct Client { float width; float height; };
 
-Client* make_client_bs(int width, int height) {
+Client* createBullshitClient(int width, int height) {
     Client* client = (Client*)calloc(1, sizeof(Client));
     client->width = width;
     client->height = height;
@@ -444,27 +427,31 @@ Client* make_client_bs(int width, int height) {
 
 void c_render(CBullshit* env) {
     if (IsKeyDown(KEY_ESCAPE)) exit(0);
-    if (env->client == NULL) env->client = make_client_bs(env->width, env->height);
+    if (env->client == NULL) env->client = createBullshitClient(env->width, env->height);
 
     BeginDrawing();
-    ClearBackground(BULL_BG);
+    ClearBackground(PUFF_BACKGROUND);
 
-    DrawText(TextFormat("Pile size: %d", env->pile_size), 20, 20, 20, BULL_WHITE);
-    DrawText(TextFormat("Last declared: %d x %d", env->last_declared_rank, env->last_declared_count), 20, 40, 20, BULL_WHITE);
+    DrawText(TextFormat("Pile size: %d", env->pile_size), 20, 20, 20, PUFF_WHITE);
+    DrawText(TextFormat("Last declared: %d x %d", env->last_declared_rank, env->last_declared_count), 20, 40, 20, PUFF_WHITE);
 
-    DrawText(TextFormat("You: %d cards", env->hand_totals[0]), 20, env->height - 80, 30, BULL_BLUE);
-    DrawText(TextFormat("Bot: %d cards", env->hand_totals[1]), env->width - 200, env->height - 80, 30, BULL_RED);
+    DrawText(TextFormat("Agent: %d cards", env->hand_totals[0]), 20, env->height - 80, 30, PUFF_CYAN);
+    DrawText(TextFormat("Bot: %d cards", env->hand_totals[1]), env->width - 200, env->height - 80, 30, PUFF_RED);
 
     /* per-rank counts for both players (debug view) */
     for (int r = 1; r <= MAX_RANK; r++) {
         int x = 20 + (r - 1) * 36;
-        DrawText(TextFormat("%d", env->hands[0][r]), x, env->height - 50, 12, BULL_WHITE);
-        DrawText(TextFormat("%d", env->hands[1][r]), x, env->height - 70, 12, BULL_WHITE);
+        DrawText(TextFormat("%d", env->hands[0][r]), x, env->height - 50, 12, PUFF_WHITE);
+        DrawText(TextFormat("%d", env->hands[1][r]), x, env->height - 70, 12, PUFF_WHITE);
     }
 
-    DrawText("Press B to call BS (manual). Otherwise agent acts automatically.", 20, env->height - 120, 12, BULL_WHITE);
+    DrawText("Press B to call BS (manual). Otherwise agent acts automatically.", 20, env->height - 120, 12, PUFF_WHITE); // TODO: DIE
 
     EndDrawing();
 }
 
-#endif /* BULLSHIT_H */
+/* stolen from TT*/
+void closeClient(Client* client) {
+    CloseWindow();
+    free(client);
+}
